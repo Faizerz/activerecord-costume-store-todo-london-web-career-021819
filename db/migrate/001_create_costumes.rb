a58884d6c607999269607f70d10ecb5c92379f93
github username <github email address>
2019-03-01 16:00:35 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,8 +2,21 @@
 
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
+# 001_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:costumes) do |t|
+      t.column :name, :string
+      t.column :price, :float
+      t.column :size, :string
+      t.column :image_url, :string
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
+    end
+  end
+end
